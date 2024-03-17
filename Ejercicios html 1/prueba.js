d6c3238d45c442af887485debb0bf101a162a971
Ibitroll <ibai.amo.ballesteros@gmail.com>
2024-03-17 23:55:36 +0100

    cambiar className con checkbox en js

--- Ejercicios html 1/prueba.js
+++ Ejercicios html 1/prueba.js
@@ -1,6 +1,13 @@
+/*Esto es para cambiar el className de lo que sea al
+hacer click sobre el checkbox:*/
 const boton = document.getElementById("check");
 const texto = document.querySelector(".tachado");
 
-
-
+boton.addEventListener('change', e =>{
+    if (boton.checked == true){
+        texto.className="notachado";
+    }else{
+        texto.className="tachado";
+    }
+});
 
