c28b382c2f45f049df839a8f57e27ef3a399af56
1AW3 User 2 <1AW3-2@zornotzalh.eus>
2024-04-15 14:45:17 +0200

    proyectos

--- Ejercicios html 1/menuBurger.js
+++ Ejercicios html 1/menuBurger.js
@@ -4,14 +4,8 @@ const menu = document.querySelector('.menuCont');
 
 let coords = document.querySelector('.showMenu');
 
-let lala = menu.getBoundingClientRect();
-
 abrir.addEventListener('click', e =>{
     menu.className="showMenu";
-    document.body.onclick = function (o){
-        console.log(o.clientX);
-    }
-
     cerrar.addEventListener('click', e =>{
         menu.className="menuCont";
     });
