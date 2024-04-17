40fe423781c9696ae7781794e9bcd40fc533fdc9
1AW3 User 2 <1AW3-2@zornotzalh.eus>
2024-04-17 14:49:59 +0200

    galeria de fotos con swipe

--- Ejercicios html 1/prueba.js
+++ Ejercicios html 1/prueba.js
@@ -1,14 +1,59 @@
-/*Esto es para cambiar el className de lo que sea al
-hacer click sobre el checkbox:*/
-const boton = document.getElementById("check");
-const texto = document.querySelector(".noshow");
+const fotos = document.querySelectorAll(".item");
+const body = document.querySelector("body");
 
-boton.addEventListener('change', e =>{
-    if (boton.checked == true){
-        texto.className="showburger";
-        
-    }else{
-        texto.className="noshow";
-    }
+let cont = 0;
+let touchstartX = 0
+let touchendX = 0
+
+fotos.forEach(foto =>{
+    foto.addEventListener('click', e =>{
+        foto.classList.toggle("verFoto");
+        quitarScroll(); 
+    });
+
+    
+        foto.addEventListener('touchstart', e => {
+            touchstartX = e.changedTouches[0].screenX
+        });
+          
+        foto.addEventListener('touchend', e => {
+            touchendX = e.changedTouches[0].screenX
+            checkDirection();
+        });
+    
+    
+   
 });
+
+function quitarScroll(){
+    if(cont %2==0){
+        body.style.overflowY="hidden";
+    }else{
+        body.style.overflowY="auto";
+    }
+    cont++;
+}
+
+function checkDirection() {
+    if (touchendX < touchstartX){
+        for(let i = 0; i < fotos.length;i++){
+            if(fotos[i].classList.contains("verFoto") && i != fotos.length-1){
+                fotos[i].classList.remove("verFoto");
+                fotos[i+1].classList.add("verFoto");
+                break;
+            }
+        }
+    }
+    if (touchendX > touchstartX){
+        for(let i = 0; i < fotos.length;i++){
+            if(fotos[i].classList.contains("verFoto") && i != 0){
+                fotos[i].classList.remove("verFoto");
+                fotos[i-1].classList.add("verFoto");
+                break;
+            }
+        }
+  }
+}
+    
+
 
