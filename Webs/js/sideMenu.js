17ee1465bf802413e5d4c67601d47c91b8c39cdf
Ibitroll <ibai.amo.ballesteros@gmail.com>
2024-05-24 00:37:18 +0200

    navBar Cinesa hecho

--- Webs/js/sideMenu.js
+++ Webs/js/sideMenu.js
@@ -1,7 +1,21 @@
 const menuAbrir = document.getElementById('menuAbrir');
 const menuCerrar = document.getElementById('menuCerrar');
 const sideMenu = document.querySelector('.sideMenu');
+const body = document.querySelector('body');
+
 
 menuAbrir.addEventListener('click', e =>{
     sideMenu.classList.add('showMenu');
+
+});
+
+body.addEventListener('click', e =>{
+    console.log(body.clientWidth - sideMenu.clientWidth)
+    if(e.clientX < body.clientWidth - sideMenu.clientWidth){
+        alert ("cerrado")
+    }
+});
+
+menuCerrar.addEventListener('click', e =>{
+    sideMenu.classList.remove('showMenu');
 });
